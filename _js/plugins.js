48c256fa7dda5ece3e3ca82de153374698e41d93
Siroberto Scerbo <github@siroberto.com>
2012-06-05 14:26:36 -0400

    updated plugins file

--- _js/plugins.js
+++ _js/plugins.js
@@ -5,7 +5,7 @@ window.log = function f(){ log.history = log.history || []; log.history.push(arg
 // make it safe to use console.log always
 (function(a){function b(){}for(var c="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),d;!!(d=c.pop());){a[d]=a[d]||b;}})
 (function(){try{console.log();return window.console;}catch(a){return (window.console={});}}());
-
+ 
 /*
  * jquery.xslt.js
  *
@@ -38,9 +38,96 @@ window.log = function f(){ log.history = log.history || []; log.history.push(arg
  * @author <a href="mailto:jb@eaio.com">Johann Burkard</a>
  * @version $Id: jquery.xslt.js,v 1.10 2008/08/29 21:34:24 Johann Exp $
  */
-eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('(2(C){C.l.h=2(){k 7};0 D=/^\\s*</;1(a.11){C.l.h=2(F,G){0 H=C(7);0 J=2(){0 K="V";1(I.5==K&&E.5==K){b.W(2(){H.z(I.P(E.Q))},R)}};0 I=a.r("6");I.d=J;I[D.f(F)?"n":"o"]=F;0 E=a.r("6");E.d=J;E[D.f(G)?"n":"o"]=G;C("T").g(I).g(E);k 7}}8{1(b.m!=e&&b.U!=e&&b.i!=e){0 B=9 i();0 A=t;1(C.v(B.j)){A=b.M!=e}8{A=q}1(A){C.l.h=2(G,H){0 I=C(7);0 F=t;0 J={5:4};0 E={5:4};0 K=2(){1(J.5==4&&E.5==4&&!F){0 L=9 i();1(C.v(L.j)){c=a.X.Y("","",y);L.j(J.3,E.3,c,y);I.z(9 M().Z(c))}8{L.10(E.3);c=L.O(J.3,a);I.S().g(c)}F=q}};1(D.f(G)){J.3=9 m().x(G,"p/6")}8{J=C.u({w:"6",N:G});J.d=K}1(D.f(H)){E.3=9 m().x(H,"p/6");K()}8{E=C.u({w:"6",N:H});E.d=K}k 7}}}}})(12);',62,65,'var|if|function|responseXML||readyState|xml|this|else|new|document|window|resultDoc|onreadystatechange|undefined|test|append|xslt|XSLTProcessor|transformDocument|return|fn|DOMParser|innerHTML|src|text|true|createElement||false|ajax|isFunction|dataType|parseFromString|null|html|||||||||||||XMLSerializer|url|transformToFragment|transformNode|XMLDocument|50|empty|body|XMLHttpRequest|complete|setTimeout|implementation|createDocument|serializeToString|importStylesheet|recalc|jQuery'.split('|'),0,{}))
+(function($) {
+    $.fn.xslt = function() {
+        return this;
+    }
+    var str = /^\s*</;
+    if (document.recalc) { // IE 5+
+        $.fn.xslt = function(xml, xslt) {
+            var target = $(this);
+                
+            var change = function() {
+                var c = 'complete';
+                if (xm.readyState == c && xs.readyState == c) {
+                        window.setTimeout(function() {
+                            target.html(xm.transformNode(xs.XMLDocument));
+                        }, 50);
+                }
+            };
 
-// place any jQuery/helper plugins in here, instead of separate, slower script files.
+            var xm = document.createElement('xml');
+            xm.onreadystatechange = change;
+            xm[str.test(xml) ? "innerHTML" : "src"] = xml;
+
+            var xs = document.createElement('xml');
+            xs.onreadystatechange = change;
+            xs[str.test(xslt) ? "innerHTML" : "src"] = xslt;
+
+            $('body').append(xm).append(xs);
+            return this;
+       };
+    }
+    else if (window.DOMParser != undefined && window.XMLHttpRequest != undefined && window.XSLTProcessor != undefined) { // Mozilla 0.9.4+, Opera 9+
+       var processor = new XSLTProcessor();
+       var support = false;
+       if ($.isFunction(processor.transformDocument)) {
+           support = window.XMLSerializer != undefined;
+       }
+       else {
+           support = true;
+       }
+       if (support) {
+            $.fn.xslt = function(xml, xslt) {
+                var target = $(this);
+                var transformed = false;
+
+                var xm = {
+                    readyState: 4
+                };
+                var xs = {
+                    readyState: 4
+                };
+
+                var change = function() {
+                    if (xm.readyState == 4 && xs.readyState == 4  && !transformed) {
+                        var processor = new XSLTProcessor();
+                        if ($.isFunction(processor.transformDocument)) {
+                            // obsolete Mozilla interface
+                            resultDoc = document.implementation.createDocument("", "", null);
+                            processor.transformDocument(xm.responseXML, xs.responseXML, resultDoc, null);
+                            target.html(new XMLSerializer().serializeToString(resultDoc));
+                        }
+                        else {
+                            processor.importStylesheet(xs.responseXML);
+                            resultDoc = processor.transformToFragment(xm.responseXML, document);
+                            target.empty().append(resultDoc);
+                        }
+                        transformed = true;
+                    }
+                };
+
+                if (str.test(xml)) {
+                    xm.responseXML = new DOMParser().parseFromString(xml, "text/xml");
+                }
+                else {
+                    xm = $.ajax({ dataType: "xml", url: xml});
+                    xm.onreadystatechange = change;
+                }
+
+                if (str.test(xslt)) {
+                    xs.responseXML = new DOMParser().parseFromString(xslt, "text/xml");
+                    change();
+                }
+                else {
+                    xs = $.ajax({ dataType: "xml", url: xslt});
+                    xs.onreadystatechange = change;
+                }
+                return this;
+            };
+       }
+    }
+})(jQuery);
 
 //xml2json from:
 /*	This work is licensed under Creative Commons GNU LGPL License.
