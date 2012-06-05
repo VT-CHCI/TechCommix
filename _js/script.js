28e69929f066950e31e7a2e3fa306f893dce0bbd
Siroberto Scerbo <github@siroberto.com>
2012-06-05 10:41:10 -0400

    working drop zone and file input button

--- _js/script.js
+++ _js/script.js
@@ -2,7 +2,56 @@
 
 */
 
+// Check for the various File API support.
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+  // Great success! All the File APIs are supported.
+} else {
+  alert('The File APIs are not fully supported in this browser.');
+}
 
+//=======================================================================================
+//
+// File handler example (http://www.html5rocks.com/en/tutorials/file/dndfiles/) for 
+// drag and drop 
+//
+//=======================================================================================
 
+function handleFileSelect(evt) {
+		if (evt.target.id == 'files') {
+			var files = evt.target.files; // FileList object
+		}
+		else {   
+			evt.stopPropagation();
+    	evt.preventDefault();
 
+    	var files = evt.dataTransfer.files; // FileList object.
+		}
 
+    // files is a FileList of File objects. List some properties.
+    var output = [];
+    for (var i = 0, f; f = files[i]; i++) {
+      output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+                  f.size, ' bytes, last modified: ',
+                  f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+                  '</li>');
+    }
+    document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+  }
+
+  function handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+  }
+
+  // Setup the dnd listeners.
+  var dropZone = document.getElementById('drop_zone');
+  dropZone.addEventListener('dragover', handleDragOver, false);
+  dropZone.addEventListener('drop', handleFileSelect, false);
+
+ window.addEventListener("drop",function(e){
+  e = e || event;
+  e.preventDefault();
+},false);
+
+  document.getElementById('files').addEventListener('change', handleFileSelect, false);
